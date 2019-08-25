a3e0c6c0a0f1ff11f2c41d8900c9f0239ba9c859
Joe <s2802053@griffithuni.edu.au>
2019-08-25 13:37:58 +1000

    Added code for master template with html framework and navigation

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <title>@yield('title')</title>
+        <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/master.css')}}">
+        @yield('style')
+    </head>
+    <body>
+        <div class="topNav">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">Week Five</a>
+                    </div>
+                    <ul style="display: inline;" class="nav navbar-nav">
+                        <li style="margin: 10px;"><a class="nav-link">Signed in as Joe</a></li>
+                        <li style="margin: 10px;"><a class="nav-link">Sign Out</a></li>
+                    </ul>   
+                </div>
+            </nav>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3">
+            <div class="sideNav">
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <ul class="nav navbar-nav">
+                            <li style="margin: 10px;"><a class="nav-link">Home</a></li>
+                            <li style="margin: 10px;"><a class="nav-link">Recent Posts</a></li>
+                            <li style="margin: 10px;"><a class="nav-link">Users</a></li>
+                        </ul>   
+                    </div>
+                </nav>
+            </div>
+            </div>
+            <div class="col-md-6">
+                @yield('content')
+            </div>
+            <div class="col-md-3"></div>
+        </div>
+    </body>
+</html>
