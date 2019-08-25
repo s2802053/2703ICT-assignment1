f929133a7e0a2f2466c330b676ebe7b505fff5f6
Joe <s2802053@griffithuni.edu.au>
2019-08-25 18:56:35 +1000

    Updated route handlers to retrieve data from database

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+    include "../includes/db_defs.php";
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,38 +15,37 @@ Route::get('/', function () {
     // home page
     // retrieve all posts from database
     // return POST_LIST view 
-    $title = "Home";
     
     // (id, author, title, message, date, icon)
-    $posts = array();
-    $posts[] = array("id"=>"1", "date"=>"3:42pm 23 Aug 2019", "author"=>"Joe", "title"=>"Post #1", "message"=>"Message of post #1", "icon"=>"C:/xampp/htdocs/2703ICT-assignment1/public/default-icon.png");
-    $posts[] = array("id"=>"2", "date"=>"3:42pm 23 Aug 2019", "author"=>"Joe", "title"=>"Post #2", "message"=>"Message of post #2", "icon"=>"C:/xampp/htdocs/2703ICT-assignment1/public/default-icon.png");
-    $posts[] = array("id"=>"3", "date"=>"3:42pm 23 Aug 2019", "author"=>"Joe", "title"=>"Post #3", "message"=>"Message of post #3", "icon"=>"C:/xampp/htdocs/2703ICT-assignment1/public/default-icon.png");
+    $sql = "select post.*, user.name from post, user where post.author = user.id";
+    $posts = DB::select($sql);
 
-    return view('post_list')->withTitle($title)->withPosts($posts);
+    return view('post_list')->withTitle("Home")->withPosts($posts);
 });
 
-Route::get('/post', function () {
+Route::get('/post/{id}', function ($id) {
     // comment page for post
     // retrieve post and associated comments from db
     // return POST view
 
-    $title = "Post";
-    
-    // (id, author, title, message, date, icon)
-    $post = array("id"=>"1", "date"=>"3:42pm 23 Aug 2019", "author"=>"Joe", "title"=>"Post #1", "message"=>"Message of post #1", "icon"=>"{{asset('./default-icon.png')}}");
-    $comments = array();
-    $comments[] = array("id"=>"1", "author"=>"Steve", "message"=>"This is comment #1");
-    $comments[] = array("id"=>"2", "author"=>"Steve", "message"=>"This is comment #2");
-    $comments[] = array("id"=>"3", "author"=>"Steve", "message"=>"This is comment #3");
+    $postSql = "select * from post where post.id = $id";
+    $posts = DB::select($postSql);
+    $post = $posts[0];
 
-    return view('post')->withTitle($title)->withPost($post)->withComments($comments);
+    $commentsSql = "select * from comment where comment.post_id = $id";
+    $comments = DB::select($commentsSql);
+
+    return view('post')->withTitle("Viewing comments for post $id")->withPost($post)->withComments($comments);
 });
 
 Route::get('/recent', function () {
     // view recent posts
     // retrieve posts from db where date is in last week
     // return POST_LIST view
+
+    $query = "select post.*, user.name from post, user where post.author = user.id AND timestamp > DATE('now', '-7 day');";
+    $result = DB::select($query);
+    return view('post_list')->withTitle("Recent Posts")->withPosts($result);
 });
 
 Route::get('/users', function(){
@@ -54,13 +53,16 @@ Route::get('/users', function(){
     // retrieve distinct usernames from table
     // return USER_LIST view
 
-    $users = array("user1", "user2", "user3", "user4", "user5");
-
+    $query = "select * from user";
+    $users = DB::select($query);
     return view('users')->withUsers($users)->withTitle("Users");
 });
 
-Route::get('user/posts', function(){
+Route::get('user_posts/{userId}', function($userId){
     // view all posts of a user
     // retrieve posts for username from db
     // return POST_LIST view
+    $query1 = "select post.*, user.name from post, user where post.author = '$userId' AND post.author = user.id";
+    $posts = DB::select($query1);
+    return view('post_list')->withTitle("$userId's posts")->withPosts($posts);
 });
