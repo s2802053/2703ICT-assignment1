61b577e243015471e0294bdaced4a61a3b35c949
Joe <s2802053@griffithuni.edu.au>
2019-08-26 15:34:33 +1000

    Added form to create a post

--- resources/views/post_list.blade.php
+++ resources/views/post_list.blade.php
@@ -14,16 +14,44 @@
 @endsection
 
 @section('content')
-    @forelse($posts as $post)
-        <div class="post">
-            <h4 class="post-author">{{ $post->name }}</h4>
-            <p class="post-timestamp">{{ $post->timestamp }}</p>
-            <img src="{{asset('default-icon.png')}}">
-            <div class="post-content">
-                <h5 class="post-title">{{ $post->title }}</h5>
-                <p class="post-message">{{ $post->message }}</p>
-            </div>
+        @if ($showCreateForm)
+        <div class="create-post">
+            <form method="get" action="/add_post" name="createPost">
+                <div id="container">
+                    <div class="topRow">
+                        <span id="username">Username</span>
+                        <br>
+                        <input type="text" placeholder="Enter username" name="username"> 
+                    </div>
+                    <div class="topRow">
+                        <span>Title</span>
+                        <br>                   
+                        <input type="text" placeholder="Enter title" name="title"> 
+                    </div>
+                </div>
+                <div id="bottomRow">
+                    Message
+                    <br>
+                    <input type="text" placeholder="Enter message" name="message">
+                </div>
+                <button type="submit">Create post</button>
+            </form>
         </div>
+        @endif
+    @forelse($posts as $post)
+        <a href="/post/{{$post->id}}">
+            <div class="post">
+                <h4 class="post-author">{{ $post->name }}</h4>
+                <p class="post-timestamp">{{ $post->timestamp }}</p>
+                <img src="{{asset('default-icon.png')}}">
+                <div class="post-content">
+                    <h5 class="post-title">{{ $post->title }}</h5>
+                    <p class="post-message">{{ $post->message }}</p>
+                </div>
+                <a href="/deletePost/{{$post->id}}">Delete Post</a>
+                <a href="/editPost/{{$post->id}}">Edit Post</a>
+            </div>
+        </a>
     @empty
         No posts found.
     @endforelse
