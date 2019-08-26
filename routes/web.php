54553a807698af3147d8515ce34516115d984876
Joe <s2802053@griffithuni.edu.au>
2019-08-26 15:37:07 +1000

    Modified route handlers to use db_defs functions to reduce code duplication and localise future changes.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
     include "../includes/db_defs.php";
+    use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,10 +18,9 @@ Route::get('/', function () {
     // return POST_LIST view 
     
     // (id, author, title, message, date, icon)
-    $sql = "select post.*, user.name from post, user where post.author = user.id";
-    $posts = DB::select($sql);
+    $posts = get_all_posts();
 
-    return view('post_list')->withTitle("Home")->withPosts($posts);
+    return view('post_list')->withTitle("Home")->withPosts($posts)->withShowCreateForm(true);
 });
 
 Route::get('/post/{id}', function ($id) {
@@ -28,14 +28,11 @@ Route::get('/post/{id}', function ($id) {
     // retrieve post and associated comments from db
     // return POST view
 
-    $postSql = "select * from post where post.id = $id";
-    $posts = DB::select($postSql);
-    $post = $posts[0];
+    $post = get_post($id);
+    $comments = get_comments($id);
 
-    $commentsSql = "select * from comment where comment.post_id = $id";
-    $comments = DB::select($commentsSql);
-
-    return view('post')->withTitle("Viewing comments for post $id")->withPost($post)->withComments($comments);
+    return view('post')->withTitle("Viewing comments for post $id")
+        ->withPost($post)->withComments($comments);
 });
 
 Route::get('/recent', function () {
@@ -43,18 +40,12 @@ Route::get('/recent', function () {
     // retrieve posts from db where date is in last week
     // return POST_LIST view
 
-    $query = "select post.*, user.name from post, user where post.author = user.id AND timestamp > DATE('now', '-7 day');";
-    $result = DB::select($query);
-    return view('post_list')->withTitle("Recent Posts")->withPosts($result);
+    $posts = get_recent_posts();
+    return view('post_list')->withTitle("Recent Posts")->withPosts($posts)->withShowCreateForm(false);
 });
 
 Route::get('/users', function(){
-    // view all unique users
-    // retrieve distinct usernames from table
-    // return USER_LIST view
-
-    $query = "select * from user";
-    $users = DB::select($query);
+    $users = get_all_users();
     return view('users')->withUsers($users)->withTitle("Users");
 });
 
@@ -62,7 +53,70 @@ Route::get('user_posts/{userId}', function($userId){
     // view all posts of a user
     // retrieve posts for username from db
     // return POST_LIST view
-    $query1 = "select post.*, user.name from post, user where post.author = '$userId' AND post.author = user.id";
-    $posts = DB::select($query1);
-    return view('post_list')->withTitle("$userId's posts")->withPosts($posts);
+    $posts = get_user_posts($userId);
+    return view('post_list')->withTitle("Posts")->withPosts($posts)->withShowCreateForm(false);
+});
+
+Route::get('/add_post', function(Request $request){
+    $input = $request->all();
+    // check if username is already a user
+    $user = get_user($input['username']);
+    if ($user){
+        $newPostId = add_post($user->id, $input['title'], $input['message']);
+    } else {
+        // create a new user
+        $newUserId = add_user($input['username']);
+        $newPostId = add_post($newUserId, $input['title'], $input['message']);
+    }
+
+    if ($newPostId){
+        return redirect("/post/$newPostId");
+    } else {
+        die('Error adding item');
+    }
+});
+
+Route::get('/add_comment', function(Request $request){
+    $input = $request->all();
+    $user = get_user($input['username']);
+    $id = $input['id'];
+    if ($user){
+        add_comment($user->id, $input['message'], $id);
+    } else {
+        // create a new user
+        $newUserId = add_user($input['username']);
+        add_comment($newUserId, $input['message'], $id);
+    }
+    return redirect("/post/$id");
+});
+
+Route::get('/test', function(){
+    $user = get_user("joe burton");
+    if ($user){
+        dump($user->id);
+    } else {
+        dump("No user");
+    }
+});
+
+Route::get('/deleteComment/{postId}/{commentId}', function($postId, $commentId){
+    del_comment($commentId);
+    return redirect("/post/$postId");
+});
+
+Route::get('/deletePost/{postId}', function($postId){
+    del_post($postId);
+    return redirect('/');
+});
+
+Route::get('/editPost/{postId}', function($postId){
+    $post = get_post($postId);
+    return view('edit_post')->withTitle("Edit post")->withPost($post);
+});
+
+Route::get('/edit_post', function(Request $request){
+    $input = $request->all();
+    $id = $request['id'];
+    update_post($id, $request['title'], $request['message']);
+    return redirect("/post/$id");
 });
